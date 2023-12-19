559ad13f3088b4d8240b463508dbcefd3bed61a3
CortenGroup <154320024+CortenGroup@users.noreply.github.com>
2023-12-19 10:35:14 +0100

    zip file and extrated file uploaded of the website

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1307-1702914705.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBTabs.init( '.uagb-block-3fd3b624' );
+	UAGBTabs.anchorTabId( '.uagb-block-3fd3b624' );
+});
+window.addEventListener( 'hashchange', function() {
+	UAGBTabs.anchorTabId( '.uagb-block-3fd3b624' );
+}, false );
+ });
